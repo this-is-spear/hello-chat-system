f178a9a5598490fee2079092c1635c5f8310635e
this-is-spear <rjsckdd12@gmail.com>
2024-05-04 17:11:40 +0900

    test : 실패하는 테스트 코드 해결

---
+++ hello-chat-server/src/main/kotlin/tis/PrometheusConfiguration.kt
@@ -0,0 +1,14 @@
+package tis
+
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class PrometheusConfiguration {
+    @Bean
+    fun prometheusMeterRegistry(): PrometheusMeterRegistry {
+        return PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    }
+}
